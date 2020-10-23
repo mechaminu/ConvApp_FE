483c854866452e4f4c11a0082a7ca31a4aa5bfa8
Minwoo <lmw5201@gmail.com>
2020-10-23 21:02:47 +0900

    피드 포스트 세부페이지 이미지 전시 문제 고침

--- ConvApp/ConvApp/Views/PostDetail.xaml.cs
+++ ConvApp/ConvApp/Views/PostDetail.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConvApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,13 @@ namespace ConvApp.Views
         public PostDetail()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            image.Source = ((Post)BindingContext).Image;
         }
     }
 }
