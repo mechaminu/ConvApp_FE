9e2148b24ac50c7c38267437249e40d806a2f3c3
Duhyun Han <gksengus1@gamil.com>
2020-11-19 16:36:19 +0900

    하트버튼

--- ConvApp/ConvApp/Views/Category/CategoryDetail.xaml.cs
+++ ConvApp/ConvApp/Views/Category/CategoryDetail.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ConvApp.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +14,26 @@ namespace ConvApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CategoryDetail : ContentPage
     {
+        int btn_count = 0;
         public CategoryDetail()
         {
             InitializeComponent();
+        }
+
+
+        private void OnClick_love(object sender, EventArgs e)
+        {
+            if (btn_count ==0)
+            {
+                btn_love.Source = ImageSource.FromResource("ConvApp.IMAGES.heartfilled.png");
+                btn_count++;
+            }
+            else 
+            {
+                btn_love.Source = ImageSource.FromResource("ConvApp.IMAGES.heart.png"); 
+                btn_count--;
+            }
+
         }
     }
 }
